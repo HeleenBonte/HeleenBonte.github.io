24985467f6b22ed24a5b756f235ad6fec972a3d2
HeleenBonte <bonteheleen@hotmail.com>
2024-03-05 21:53:49 +0100

    labo3

--- TweedeSemester/Labo2/Opdracht8/scripts/code.js
+++ TweedeSemester/Labo2/Opdracht8/scripts/code.js
@@ -1,18 +1,13 @@
 const setup = () => {
-
     let btnSubstring = document.getElementById("btnSubstring");
-
-
     btnSubstring.addEventListener("click", txtSubstring);
 }
-const txtSubstring = () =>{
+const txtSubstring = () => {
     let txtInput = document.getElementById("txtInput");
-    if(txtInput.value != ""){
-        let ondergrens = document.getElementById("ondergrens");
-        let bovengrens = document.getElementById("bovengrens");
-        let tekst = txtInput.value.substring(ondergrens.value, bovengrens.value);
-        let txtOutput = document.getElementById("txtOutput");
-        txtOutput.innerHTML = tekst;
-    }
+    let ondergrens = document.getElementById("ondergrens");
+    let bovengrens = document.getElementById("bovengrens");
+    let tekst = txtInput.value.substring(ondergrens.value, bovengrens.value);
+    let txtOutput = document.getElementById("txtOutput");
+    txtOutput.innerHTML = tekst;
 }
 window.addEventListener("load", setup);
